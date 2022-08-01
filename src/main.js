55accb5bca72171be3a8cb0f895d0eb8a8d4ae20
ConniZMac <constanza.macnamara@gmail.com>
2022-07-31 22:35:44 -0400

    cambios 3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-/*import App from './components/App.js';
+import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());*/
+document.getElementById('root').appendChild(App());
 
-const cards = document.querySelectorAll('.memory-card')
-function flipCard(){
-    this.classList.('flip');
-}
-cards.forEach(card=> card.addEventListener)('click',flipCard);
